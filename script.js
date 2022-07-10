0b98e782d49e185cb27971acf3bc96c6a41f2991
GPA <gabrielproencaaraujo@gmail.com>
2022-07-10 13:57:21 -0300

    alteração na estrutura

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log(asdasd);
+const map = L.map('map').setView([-24.0104, -48.8564], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap',
+}).addTo(map);
